6152bf468dd6893904e98ee5813fd27233a0f6fc
jacob-et-israel <israeletjacob@gmail.com>
2025-07-21 11:43:33 +0000

    Updated env.js

---
+++ api/src/config/env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://jobdaniel2025:Session2025@cluster0.jwccoxl.mongodb.net/leonce-studio?retryWrites=true&w=majority&appName=Cluster0',
+  NODE_ENV: 'development',
+  PORT: 5000,
+  JWT_SECRET: 'your-super-secret-jwt-key'
+};
